7312f25f1061b86c9e3081984a6f2ccc78836a61
Vaiz <vaizerd@gmail.com>
2014-10-01 21:48:35 +0400

    Класс Label

---
+++ XNA_test1/XNA_test1/Label.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XNA_test1
+{
+    class Label
+    {
+        #region Переменные
+
+        string text;            // текст сообщения
+        Color color;
+        SpriteFont font;        // шрифт текста       
+        int x;              
+        int y;
+        
+        #endregion
+        //==============================================================================================
+        #region Инициализация
+
+        public Label()
+        {
+            
+        }
+       
+        public SpriteFont Font
+        {
+            get { return font; }
+            set
+            {
+                font = value;
+            }
+        }
+
+        public int X
+        {
+            get { return x; }
+            set 
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set 
+            {
+                y = value;
+            }
+        }
+
+        public string Text
+        {
+            set
+            {
+                text = value;
+            //    UpdateButtonPosition();
+            }
+        }
+
+        public Color Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        #endregion
+        //==============================================================================================
+        #region Основные потоки
+
+        public void Update(GameTime time)
+        {
+            
+        }
+
+        public void Draw(SpriteBatch bath)
+        {
+            bath.DrawString(font, text, new Vector2(x, y), color);
+        }
+
+        #endregion
+    }
+}
